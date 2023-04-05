3e493a3e0a51fec7e720420c9136ace6e5ed723c
me <me@c0e59bd770920612ccc9e2a8857cff36b66833e9507eb973149ce8d93cab5fff>
2023-04-05 13:21:50 +0200

    Initial Commit

---
+++ scanNet/scanNet.py
@@ -0,0 +1,41 @@
+import subprocess
+
+class scanNet():
+    def __init__(self):
+        # Initializes the scan, storing it in instance variable scan
+        self.address = "192.168.0.1/24"
+        self.scan = subprocess.check_output(f'sudo nmap -sP {self.address}', shell=True)
+        # print(self.scan)
+    
+    def parseOutput(self):
+        # Taks the output of nmap, splitting it into a 2D List storing information of each device
+        scan = str(self.scan).split("\\n")
+        # print(scan)
+        addressId = self.address[:int(len(self.address) / 2)]
+        print(addressId)
+        addrList = []
+        count = 0
+        for device in scan:
+            if count % 2 == 0:
+                tmpList = []
+            print(device)
+            if device.__contains__(addressId):
+                tmpList.append('Ip Address')
+                tmpList.append(device[(device.rindex(" ") + 1):])
+            if device.__contains__("Host"):
+                tmpList.append('Latency')
+                tmpList.append(device[(device.index("(") + 1):device.rindex("s")] + 's')
+
+            if tmpList:
+                addrList.append(tmpList)
+        print(addrList)
+        self.addrList = addrList
+
+    def detailedScan(self):
+        # Runs a detailed scan on all devices. This scan may take up to 10 seconds per device, as such it is NOT RECCOMMENDED ON BIG NETWORKS
+        for device in self.addrList:
+            ipAddr = device[]
+
+
+scanNet().parseOutput()
+scanNet().detailedScan()
